cd7e43cf54548012e0cb65faf065d160d90de240
muhammedsheriff <muhammed.sheriff9999@gmail.com>
2023-01-09 18:22:18 +0200

    commit the task

---
+++ src/configs/navigationConfig.js
@@ -0,0 +1,14 @@
+import React from "react"
+import * as Icon from "react-feather"
+const navigationConfig = [
+  {
+    id: "home",
+    title: "Home",
+    type: "item",
+    icon: <Icon.Home size={20} />,
+    permissions: ["admin", "editor"],
+    navLink: "/"
+  },
+]
+
+export default navigationConfig
